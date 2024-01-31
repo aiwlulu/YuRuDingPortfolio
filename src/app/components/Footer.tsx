43c363a6e59eceb7d1a4ca4c92f2069d8b221322
Lulu <s9341729@gmail.com>
2024-01-31 23:23:57 +0800

    Update footer to include border-bottom transparency

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white flex items-center justify-center">
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent flex items-center justify-center">
       <div className="container p-12 text-center">
         <p className="text-slate-600">Yu Ru Ding © 2024</p>
       </div>
